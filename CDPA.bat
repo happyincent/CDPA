1c47d44cdccc7d5e3a5886a0174b1054e1d9977d
Vincent Chen <happyincent@gmail.com>
2016-03-16 14:50:52 +0800

    Added files via upload

--- CDPA.bat
+++ CDPA.bat
@@ -1,8 +1,8 @@
 ::/*Author：DDL
-:: *Date：Mar. 7, 2016    */
+:: *Date：Mar. 16, 2016    */
 
 @ECHO off
-SETLOCAL ENABLEDELAYEDEXPANSION 
+SETLOCAL 
 chcp 65001
 SET interface_name="乙太網路"
 
@@ -35,7 +35,7 @@ if '%errorlevel%' NEQ '0' (
     ECHO Set IP
     ECHO.
     SET /p "dorm_num=Which Dorm ('Upper case' ABCDEFGHL, 1234, T:Go test, Q:Exit)? " || GOTO choose_dorm
-    if %dorm_num%==T CLS & GOTO ping_test
+    if %dorm_num%==T SET if_set_gateway=0 & CLS & GOTO ping_test
     if %dorm_num%==Q GOTO END
     GOTO set_Sub_Mask
     
@@ -54,6 +54,7 @@ if '%errorlevel%' NEQ '0' (
         if %%a==%dorm_num%_Gate (
             ECHO D_Gate: %%b
             SET D_Gate=%%b
+            SET if_set_gateway=1
             GOTO choose_room
         )
     )
@@ -86,13 +87,17 @@ if '%errorlevel%' NEQ '0' (
 :ping_test
     ECHO Ping Test Opts
     ECHO.
-    ECHO 1.ping 8.8.8.8       2.ping fb.com     3.ipconfig /all
+    ECHO 1.ping 8.8.8.8     2.ping fb.com     3.ping "parameters"
     ECHO.
-    ECHO 4.ping "parameters"  5.Back to Set IP           6.Exit
+    ECHO 4.ping gateway     5.ping arista     6.ipconfig /all
     ECHO.
-    CHOICE /C 123456
-    IF ERRORLEVEL 6 GOTO END
-    IF ERRORLEVEL 5 GOTO choose_dorm
+    ECHO 7.Back to Set IP                     8.Exit
+    ECHO.
+    CHOICE /C 12345678
+    IF ERRORLEVEL 8 GOTO END
+    IF ERRORLEVEL 7 GOTO choose_dorm
+    IF ERRORLEVEL 6 CLS & GOTO test6
+    IF ERRORLEVEL 5 CLS & GOTO test5
     IF ERRORLEVEL 4 CLS & GOTO test4
     IF ERRORLEVEL 3 CLS & GOTO test3
     IF ERRORLEVEL 2 CLS & GOTO test2
@@ -103,16 +108,25 @@ if '%errorlevel%' NEQ '0' (
     ECHO.
     GOTO ping_test
     
-:test4
-    ECHO.
-    SET /p ping_cmd=Enter command: ping 
-    %SystemRoot%\system32\ping.exe %ping_cmd%
-    pause && CLS & GOTO ping_test
-
-:test3
+:test6
     %SystemRoot%\system32\ipconfig /all
     pause && CLS & GOTO ping_test
     
+:test5
+    %SystemRoot%\system32\ping.exe 140.117.232.69
+    pause && CLS & GOTO ping_test
+    
+:test4
+    if %if_set_gateway%==0 ECHO haven't set deafault gateway & ECHO. & GOTO ping_test
+    %SystemRoot%\system32\ping.exe %D_Gate%
+    pause && CLS & GOTO ping_test
+    
+:test3
+    ECHO.
+    SET /p "ping_cmd=Enter command: ping " || GOTO test3
+    %SystemRoot%\system32\ping.exe %ping_cmd%
+    pause && CLS & GOTO ping_test
+   
 :test2
     %SystemRoot%\system32\ping.exe fb.com
     pause && CLS & GOTO ping_test    
