38e20e9c1a5e7604e26b7d6d91603acba595875a
Vincent Chen <happyincent@gmail.com>
2016-04-27 01:02:32 +0800

    Added files via upload

--- CDPA.bat
+++ CDPA.bat
@@ -1,12 +1,12 @@
 ::/*Author：DDL
-:: *Date：April. 26, 2016    */
+:: *Date：April. 27, 2016    */
 
 @ECHO off
 SETLOCAL
 chcp 65001
 SET interface_name="乙太網路"
 
-::---------------GotAdmin---------------
+::-------------------GotAdmin-------------------
 REM --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
@@ -40,13 +40,10 @@ if '%errorlevel%' NEQ '0' (
     ECHO 3.Exit
     ECHO.
     CHOICE /C 123
-    IF ERRORLEVEL 3 CLS & GOTO END
-    IF ERRORLEVEL 2 CLS & GOTO ping_test
+    IF ERRORLEVEL 3 GOTO END
+    IF ERRORLEVEL 2 GOTO ping_test
     IF ERRORLEVEL 1 CLS & GOTO choose_dorm
     ::choice fail
-    CLS
-    ECHO Error input, please input again.
-    ECHO.
     GOTO main
 ::--------------------------------------
 
@@ -58,7 +55,7 @@ if '%errorlevel%' NEQ '0' (
     ECHO /* The following choice is Case-Insensitive */
     ECHO.
     SET /p "dorm_num=Which Dorm (ABCDEFGHL, 1234, T:Go "Ping Test", Q:Exit)? " || GOTO choose_dorm
-    if /I %dorm_num%==T CLS & GOTO ping_test
+    if /I %dorm_num%==T GOTO ping_test
     if /I %dorm_num%==Q GOTO END
     GOTO set_Sub_Mask
     
@@ -99,17 +96,18 @@ if '%errorlevel%' NEQ '0' (
     netsh interface ip set address name=%interface_name% static %IP_Addr% %Sub_Mask% %D_Gate% 1
     pause
     IF ERRORLEVEL 1 GOTO if_set_again
-    IF ERRORLEVEL 0 ipconfig /renew && CLS & GOTO ping_test
+    IF ERRORLEVEL 0 ipconfig /renew && GOTO ping_test
     
 :if_set_again
     CHOICE /C YN /M "Error input, set again(Y), go test(N)?"
-    IF ERRORLEVEL 2 CLS & GOTO ping_test
+    IF ERRORLEVEL 2 GOTO ping_test
     IF ERRORLEVEL 1 GOTO choose_dorm
 ::--------------------------------------
 
 
 ::-------------------Ping Test-------------------
 :ping_test
+    CLS
     ECHO Ping Test Opts
     ECHO.
     ECHO 1.ping 8.8.8.8     2.ping fb.com     3.ping "parameters"
@@ -128,37 +126,34 @@ if '%errorlevel%' NEQ '0' (
     IF ERRORLEVEL 2 CLS & GOTO test2
     IF ERRORLEVEL 1 CLS & GOTO test1
     ::choice fail
-    CLS
-    ECHO Error input, please input again.
-    ECHO.
     GOTO ping_test
     
 :test6
     %SystemRoot%\system32\ipconfig /all
-    pause && CLS & GOTO ping_test
+    pause && GOTO ping_test
     
 :test5
     %SystemRoot%\system32\ping.exe 140.117.232.69
-    pause && CLS & GOTO ping_test
+    pause && GOTO ping_test
     
 :test4
-    if %if_set_gateway%==0 ECHO haven't set deafault gateway & ECHO. & GOTO ping_test
+    if %if_set_gateway%==0 ECHO haven't set deafault gateway & ECHO. & pause & GOTO ping_test
     %SystemRoot%\system32\ping.exe %D_Gate%
-    pause && CLS & GOTO ping_test
+    pause && GOTO ping_test
     
 :test3
     ECHO.
     SET /p "ping_cmd=Enter command: ping " || GOTO test3
     %SystemRoot%\system32\ping.exe %ping_cmd%
-    pause && CLS & GOTO ping_test
+    pause && GOTO ping_test
    
 :test2
     %SystemRoot%\system32\ping.exe fb.com
-    pause && CLS & GOTO ping_test    
+    pause && GOTO ping_test    
     
 :test1
     %SystemRoot%\system32\ping.exe 8.8.8.8
-    pause && CLS & GOTO ping_test
+    pause && GOTO ping_test
 ::--------------------------------------
 
 :END
